eb4422591a446c77a215403bef7da5c449555f1b
Hector Luis <ticaje@free-social.net>
2012-10-04 00:04:25 +0200

    Add sidebar_partial generator

---
+++ lib/generators/qadmin/sidebar_partial/sidebar_partial_generator.rb
@@ -0,0 +1,8 @@
+class Qadmin::SidebarPartialGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../../../../../app/views/admin/base', __FILE__)
+
+  def copy_index_view_files
+    say "Writing sidebar partial for #{name}", :green
+    copy_file '_sidebar.html.erb', "app/views/admin/#{name}/_sidebar.html.erb"
+  end
+end
