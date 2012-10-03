5f509b41b16a0ace99b40a3ca458cb545069a72b
Hector Luis <ticaje@free-social.net>
2012-10-04 01:34:08 +0200

    Use draper decorators if available

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,4 +4,22 @@ class Admin::BaseController < InheritedResources::Base
 
   helper Qadmin::ApplicationHelper
 
+  protected
+
+  def collection
+    get_collection_ivar || set_collection_ivar(decorate_resource_or_collection(end_of_association_chain.scoped))
+  end
+
+  def resource
+    get_resource_ivar || set_resource_ivar(decorate_resource_or_collection(super))
+  end
+
+  private
+
+  def decorate_resource_or_collection(item_or_items)
+    (resource_class.name + "Decorator").constantize.decorate(item_or_items)
+  rescue NameError
+    item_or_items
+  end
+
 end
