baf451325bc81ccebfc0283961db29a9e19ff658
Hector Luis <ticaje@free-social.net>
2012-10-03 21:38:24 +0200

    Correctly place generators in qadmin namespace

--- lib/generators/qadmin/resource_views/resource_views_generator.rb
+++ lib/generators/qadmin/resource_views/resource_views_generator.rb
@@ -2,6 +2,7 @@ class Qadmin::ResourceViewsGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../../../../../app/views/admin', __FILE__)
 
   def copy_view_files
+    say "Writing views for #{name}", :green
     directory "base", "app/views/admin/#{name}/"
   end
 end
--- lib/generators/resource_edit_view/resource_edit_view_generator.rb
+++
@@ -1,3 +0,0 @@
-class ResourceEditViewGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
-end
