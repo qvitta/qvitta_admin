72a9f09294ec2d934ef83c0452a6bd12b27ddda0
Jorge Calás Lozano <calas@qvitta.net>
2012-10-10 14:58:13 +0200

    Makes resource_show_view generator work

--- lib/generators/q_admin/resource_show_view/resource_show_view_generator.rb
+++ lib/generators/q_admin/resource_show_view/resource_show_view_generator.rb
@@ -1,3 +1,7 @@
 class QAdmin::ResourceShowViewGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
+  source_root File.expand_path('../../../../../app/views/admin/base', __FILE__)
+
+  def copy_resource_collection_partial
+    copy_file "show.html.erb", "app/views/admin/#{name}/show.html.erb"
+  end
 end
